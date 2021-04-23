4c3107176ee1262a613222a1146762fed13f4ce7
SHILTON PEREIRA SANTIAGO - 900000968 <shilton.santiago@animaeducacao.com.br>
2021-04-23 16:47:39 -0300

    Configuring node and mysql

--- node/index.js
+++ node/index.js
@@ -1,6 +1,22 @@
 const express = require('express')
+const mysql = require ('mysql')
+require('dotenv').config()
 const app = express()
 const port = 3000
+
+const config = {
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASS,
+    database: process.env.DB_DATABASE
+};
+
+const connection = mysql.createConnection(config)
+
+const sqlInsert = `INSERT INTO tb_people (name) values ('Shilton')`
+connection.query(sqlInsert)
+connection.end()
+
 
 app.get('/',(req,res) => {
     res.send('Node - Shilton');
